cd6f7667e6f510ce613409be9020e1ce320cf234
Julien Mageau <julien.mageau@live.ca>
2019-08-27 00:50:36 -0400

    Added command line arguments and progress bar

--- src/image_functions.rs
+++ src/image_functions.rs
@@ -1,27 +1,26 @@
-use image::{
-    RgbImage,
-    Pixel,
-    Rgb
-};
-use rand::seq::SliceRandom;
-use std::collections::VecDeque;
-use std::collections::HashMap;
-use std::hash::BuildHasherDefault;
-use std::fs::create_dir_all;
-use crate::color_functions::Color;
 use crate::color_functions::color_distance;
-
+use crate::color_functions::Color;
+use image::{Pixel, Rgb, RgbImage};
+use rand::seq::SliceRandom;
+use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::fs::create_dir_all;
+use std::hash::BuildHasherDefault;
 
 #[derive(Clone, Eq, PartialEq, Hash)]
 struct Point {
     x: u32,
-    y: u32
+    y: u32,
 }
 
-
-pub fn generate_image(size_x: u32, size_y: u32, directory: &str) {
+pub fn generate_image(
+    size_x: u32,
+    size_y: u32,
+    directory: &str,
+    progress_bar: &indicatif::ProgressBar,
+) {
     let mut random_colors = random_colors(size_x * size_y);
-    place_pixels(&mut random_colors, size_x, size_y, directory);
+    place_pixels(&mut random_colors, size_x, size_y, directory, &progress_bar);
 }
 
 fn random_colors(count: u32) -> VecDeque<Color> {
@@ -30,7 +29,11 @@ fn random_colors(count: u32) -> VecDeque<Color> {
     for r in (0..256).step_by(color_step) {
         for g in (0..256).step_by(color_step) {
             for b in (0..256).step_by(color_step) {
-                colors.push_back(Color::new((r + color_step) as u8, (g + color_step) as u8, (b + color_step) as u8));
+                colors.push_back(Color::new(
+                    (r + color_step) as u8,
+                    (g + color_step) as u8,
+                    (b + color_step) as u8,
+                ));
             }
         }
     }
@@ -38,15 +41,39 @@ fn random_colors(count: u32) -> VecDeque<Color> {
     colors
 }
 
-fn place_pixels(colors: &mut VecDeque<Color>, size_x: u32, size_y: u32, directory: &str) {
+fn place_pixels(
+    colors: &mut VecDeque<Color>,
+    size_x: u32,
+    size_y: u32,
+    directory: &str,
+    progress_bar: &indicatif::ProgressBar,
+) {
     create_dir_all(&directory).unwrap();
     let image_interval = size_x * size_y / 512;
 
-    let mut pixels = HashMap::with_capacity_and_hasher((size_x * size_y) as usize, BuildHasherDefault::<fnv::FnvHasher>::default());
+    let mut pixels = HashMap::with_capacity_and_hasher(
+        (size_x * size_y) as usize,
+        BuildHasherDefault::<fnv::FnvHasher>::default(),
+    );
     // Pixels with at least one free neighbour
-    let mut active_pixels = HashMap::with_capacity_and_hasher(((size_x + size_y) * 2) as usize, BuildHasherDefault::<fnv::FnvHasher>::default());
+    let mut active_pixels = HashMap::with_capacity_and_hasher(
+        ((size_x + size_y) * 2) as usize,
+        BuildHasherDefault::<fnv::FnvHasher>::default(),
+    );
 
-    add_pixel(Point { x: size_x / 2, y: size_y / 2 }, colors.pop_front().unwrap(), &mut pixels, &mut active_pixels, size_x, size_y);
+    add_pixel(
+        Point {
+            x: size_x / 2,
+            y: size_y / 2,
+        },
+        colors.pop_front().unwrap(),
+        &mut pixels,
+        &mut active_pixels,
+        size_x,
+        size_y,
+    );
+    progress_bar.inc(1);
+
     create_image(&pixels, size_x, size_y, &directory, "img0");
 
     let mut color_distance_threshold = 2;
@@ -57,11 +84,21 @@ fn place_pixels(colors: &mut VecDeque<Color>, size_x: u32, size_y: u32, director
         let best_point = get_best_point(&color, &active_pixels, color_distance_threshold);
         if best_point.is_some() {
             let free_neighbours = free_neighbours(&best_point.unwrap(), &pixels, size_x, size_y);
-            let point = free_neighbours.choose(&mut rand::thread_rng()).unwrap().clone();
-            add_pixel(point, color, &mut pixels, &mut active_pixels, size_x, size_y);
+            let point = free_neighbours
+                .choose(&mut rand::thread_rng())
+                .unwrap()
+                .clone();
+            add_pixel(
+                point,
+                color,
+                &mut pixels,
+                &mut active_pixels,
+                size_x,
+                size_y,
+            );
+            progress_bar.inc(1);
             colors_counter = 0;
             if (pixels.len() as u32 - 1) % image_interval == 0 {
-                println!("{}, {}, {}", colors.len(), active_pixels.len() - 1, color_distance_threshold);
                 create_image(&pixels, size_x, size_y, &directory, &format!("img{}", i));
                 i += 1;
                 color_distance_threshold -= 1;
@@ -81,11 +118,19 @@ fn place_pixels(colors: &mut VecDeque<Color>, size_x: u32, size_y: u32, director
     create_image(&pixels, size_x, size_y, &directory, "0final");
 }
 
-fn add_pixel(point: Point, color: Color, pixels: &mut HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>, active_pixels: &mut HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>, size_x: u32, size_y: u32) {
+fn add_pixel(
+    point: Point,
+    color: Color,
+    pixels: &mut HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>,
+    active_pixels: &mut HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>,
+    size_x: u32,
+    size_y: u32,
+) {
     pixels.insert(point.clone(), color.clone());
     active_pixels.insert(point, color);
 
-    let active_points_to_remove = active_pixels.iter()
+    let active_points_to_remove = active_pixels
+        .iter()
         .map(|(p, _)| p.clone())
         .filter(|p| free_neighbours(p, &pixels, size_x, size_y).len() == 0)
         .collect::<Vec<_>>();
@@ -94,28 +139,61 @@ fn add_pixel(point: Point, color: Color, pixels: &mut HashMap<Point, Color, Buil
     }
 }
 
-fn get_best_point(color: &Color, active_pixels: &HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>, color_distance_threshold: u32) -> Option<Point> {
-    active_pixels.iter().find(|&(_, c)| color_distance(color, c) < color_distance_threshold).map(|(p, _)| p.clone())
+fn get_best_point(
+    color: &Color,
+    active_pixels: &HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>,
+    color_distance_threshold: u32,
+) -> Option<Point> {
+    active_pixels
+        .iter()
+        .find(|&(_, c)| color_distance(color, c) < color_distance_threshold)
+        .map(|(p, _)| p.clone())
 }
 
-fn free_neighbours(point: &Point, pixels: &HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>, size_x: u32, size_y: u32) -> Vec<Point> {
+fn free_neighbours(
+    point: &Point,
+    pixels: &HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>,
+    size_x: u32,
+    size_y: u32,
+) -> Vec<Point> {
     let mut neighbours = vec![];
     if point.y > 0 {
-        neighbours.push(Point { x: point.x, y: point.y - 1 });
+        neighbours.push(Point {
+            x: point.x,
+            y: point.y - 1,
+        });
     }
     if point.x < size_x - 1 {
-        neighbours.push(Point { x: point.x + 1, y: point.y });
+        neighbours.push(Point {
+            x: point.x + 1,
+            y: point.y,
+        });
     }
     if point.y < size_y - 1 {
-        neighbours.push(Point { x: point.x, y: point.y + 1 });
+        neighbours.push(Point {
+            x: point.x,
+            y: point.y + 1,
+        });
     }
     if point.x > 0 {
-        neighbours.push(Point { x: point.x - 1, y: point.y });
+        neighbours.push(Point {
+            x: point.x - 1,
+            y: point.y,
+        });
     }
-    neighbours.into_iter().filter(|p| !pixels.contains_key(p)).collect()
+    neighbours
+        .into_iter()
+        .filter(|p| !pixels.contains_key(p))
+        .collect()
 }
 
-fn create_image(pixels: &HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>, size_x: u32, size_y: u32, directory: &str, filename: &str) {
+fn create_image(
+    pixels: &HashMap<Point, Color, BuildHasherDefault<fnv::FnvHasher>>,
+    size_x: u32,
+    size_y: u32,
+    directory: &str,
+    filename: &str,
+) {
     let mut img = RgbImage::new(size_x, size_y);
     for (p, c) in pixels {
         img.put_pixel(p.x, p.y, Rgb::from_channels(c.r, c.g, c.b, 0));
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,69 @@
 mod color_functions;
 mod image_functions;
 
-fn main() {
-    let size_x: u32 = 256;
-    let size_y: u32 = 128;
+use structopt::StructOpt;
 
-    println!("Generating {}x{} image", size_x, size_y);
-    let directory = format!("output/{}_{}x{}", chrono::Local::now().timestamp(), size_x, size_y);
-    let duration = chrono::Duration::span(|| image_functions::generate_image(size_x, size_y, &directory));
-    create_info_file(&directory, size_x, size_y, duration);
-    println!("Complete");
+#[derive(StructOpt, Debug)]
+#[structopt(name = "colors", about = "Generates colourful images")]
+struct Opt {
+    #[structopt(help = "The image width.")]
+    width: u32,
+    #[structopt(help = "The image height.")]
+    height: u32,
+    #[structopt(default_value = "0.5", help = "The horizontal starting point.")]
+    horizontal_start_point: f32,
+    #[structopt(default_value = "0.5", help = "The vertical starting point.")]
+    vertical_start_point: f32,
+    #[structopt(
+        short = "o",
+        long = "output",
+        default_value = "ColorsOutput",
+        help = "The output directory."
+    )]
+    output_directory: String,
 }
 
-fn create_info_file(directory: &str, size_x: u32, size_y: u32, duration: chrono::Duration) {
+fn get_progress_bar(length: u64) -> indicatif::ProgressBar {
+    let progress_bar = indicatif::ProgressBar::new(length);
+    progress_bar.set_style(
+        indicatif::ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {msg} {wide_bar:0.cyan/blue} {pos}/{len} {eta}")
+            .progress_chars("#>-"),
+    );
+    progress_bar
+}
+
+fn main() {
+    let opt: Opt = Opt::from_args();
+    let directory = format!(
+        "output/{}_{}x{}",
+        chrono::Local::now().timestamp(),
+        opt.width,
+        opt.height
+    );
+    let progress_bar = get_progress_bar((opt.width * opt.height).into());
+    progress_bar.set_message(&format!("Creating {}x{} image", opt.width, opt.height));
+
+    let start_time = std::time::Instant::now();
+    // TODO: Update progress bar
+    image_functions::generate_image(opt.width, opt.height, &directory, &progress_bar);
+    let duration = start_time.elapsed();
+
+    progress_bar.finish_with_message(&format!("Created {}x{} image", opt.width, opt.height));
+
+    create_info_file(&directory, opt.width, opt.height, &duration);
+}
+
+fn create_info_file(directory: &str, size_x: u32, size_y: u32, duration: &std::time::Duration) {
     let color_step = (256f32 / ((size_x * size_y) as f32).cbrt()) as u16;
-    let contents = format!("Dimensions: {}x{}\nColor step: {}\nTime to complete: {}.{} seconds",
-                           size_x, size_y, color_step, duration.num_seconds(), duration.num_milliseconds());
+    let contents = format!(
+        "Dimensions: {}x{}\nColor step: {}\nTime to complete: {}.{} seconds",
+        size_x,
+        size_y,
+        color_step,
+        duration.as_secs(),
+        duration.subsec_millis()
+    );
 
     std::fs::write(format!("{}/0info.txt", directory), contents).unwrap();
 }
